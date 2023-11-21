362276999e0728416fd858e1e7f7038c17792c1a
Joao.Macosso <joaoclaudiomacosso@gmail.com>
2023-11-21 19:15:48 +0100

    Update riskMeasures.R

--- R/riskMeasures.R
+++ R/riskMeasures.R
@@ -1,18 +1,24 @@
+library(quantmod)
+library(xts)
+library(zoo)
+library(R6)
+library(stats)
 
+#' @export
 VaR <- function(data, return.type = "log", interval = "d", alpha = 0.05, dist = "quantile"){
   if(return.type == "log"){
-    return = na.omit(diff(log(data)))
+    return = stats::na.omit(diff(log(data)))
   }
 
   miu <- sapply(return, mean)
-  sigma <- sapply(return, sd)
+  sigma <- sapply(return, stats::sd)
 
   n = length(return[,1])
 
-  if(dist=="quantile") VaRs <- sapply(return, quantile, alpha)
+  if(dist=="quantile") VaRs <- sapply(return, stats::quantile, alpha)
   else if(dist=="normal"){
     dist <- normalize(return)
-    VaRs <- sapply(dist, quantile, alpha)
+    VaRs <- sapply(dist, stats::quantile, alpha)
   }
 
   return(VaRs)
