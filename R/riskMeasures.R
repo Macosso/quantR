9fc55fd991f3ffd85f93803de8520511e08381c5
Joao.Macosso <joaoclaudiomacosso@gmail.com>
2023-11-22 23:58:39 +0100

    Update riskMeasures.R

--- R/riskMeasures.R
+++ R/riskMeasures.R
@@ -1,31 +1,88 @@
-# library(quantmod)
-# library(xts)
-# library(zoo)
-# library(R6)
-# library(stats)
-
+#' Calculate Value at Risk (VaR) for a set of returns.
+#'
+#' This function computes VaR using either quantiles or a normal distribution
+#' assumption. It can handle missing values and supports different confidence
+#' levels and distribution assumptions.
+#'
+#' @param returns Numeric matrix or data frame of returns. Rows represent
+#' observations, and columns represent different assets or portfolios.
+#' @param alpha Confidence level for VaR, typically between 0 and 1. Default is 0.05.
+#' @param dist Distribution assumption for VaR calculation. Options are
+#' "quantile" (default) for quantile-based VaR or "normal" for normal distribution
+#' assumption.
+#'
+#' @return A numeric vector or matrix of VaR values corresponding to each column
+#' in the input returns.
+#' @examples
+#' \dontrun{
+#' library(xts)
+#' # Assuming xts objects
+#' returns_xts <- xts(matrix(c(0.01, -0.02, 0.03, -0.01, 0.02, -0.03), ncol = 2),
+#'                    order.by = as.Date(1:6))
+#'
+#' # Calculate quantile-based VaR
+#' VaR(returns_xts, alpha = 0.05, dist = "quantile")
+#'
+#' # Calculate VaR assuming normal distribution
+#' VaR(returns_xts, alpha = 0.05, dist = "normal")
+#' }
+#'
 #' @export
-VaR <- function(returns, alpha = 0.05, dist = "quantile"){
-  returns = stats::na.omit(returns)
+#'
+#' @rdname VaR
+VaR <- function(returns, alpha = 0.05, dist = "quantile") {
+  returns <- stats::na.omit(returns)
 
   miu <- sapply(returns, mean)
   sigma <- sapply(returns, stats::sd)
 
-  n = length(returns[,1])
+  n <- nrow(returns)
 
-  if(dist=="quantile") VaRs <- sapply(returns, stats::quantile, alpha)
-  else if(dist=="normal"){
-    VaRs <-  miu + qnorm(alpha)*sigma
+  if (dist == "quantile") {
+    VaRs <- sapply(returns, stats::quantile, alpha)
+  } else if (dist == "normal") {
+    VaRs <- miu + stats::qnorm(alpha) * sigma
   }
 
   return(VaRs)
 }
 
-
-CVaR <- function(returns, alpha = 0.05, dist = "quantile"){
+#' Calculate Conditional Value at Risk (CVaR) for a set of returns.
+#'
+#' This function computes CVaR using either quantiles or a normal distribution
+#' assumption. It can handle missing values and supports different confidence
+#' levels and distribution assumptions.
+#'
+#' @param returns Numeric matrix or data frame of returns. Rows represent
+#' observations, and columns represent different assets or portfolios.
+#' @param alpha Confidence level for CVaR, typically between 0 and 1. Default is 0.05.
+#' @param dist Distribution assumption for CVaR calculation. Options are
+#' "quantile" (default) for quantile-based CVaR or "normal" for normal distribution
+#' assumption.
+#'
+#' @return A numeric vector or matrix of CVaR values corresponding to each column
+#' in the input returns.
+#'
+#' @examples
+#' \dontrun{
+#' library(xts)
+#' # Assuming xts objects
+#' returns_xts <- xts(matrix(c(0.01, -0.02, 0.03, -0.01, 0.02, -0.03), ncol = 2),
+#'                    order.by = as.Date(1:6))
+#'
+#' # Calculate quantile-based CVaR
+#' CVaR(returns_xts, alpha = 0.05, dist = "quantile")
+#'
+#' # Calculate CVaR assuming normal distribution
+#' CVaR(returns_xts, alpha = 0.05, dist = "normal")
+#' }
+#'
+#' @export
+#'
+#' @rdname CVaR
+CVaR <- function(returns, alpha = 0.05, dist = "quantile") {
   vars <- quantR::VaR(returns, alpha = alpha, dist = dist)
   Xs <- as.list(as.data.frame(returns))
-  cvars <- mapply(function(x,y) mean(x[x<=y], na.rm = T), Xs, vars )
+  cvars <- mapply(function(x, y) mean(x[x <= y], na.rm = TRUE), Xs, vars)
   return(cvars)
 }
-
